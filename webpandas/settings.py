fd2733fdc646b2ecd2bd6ed2bb3784c175a406eb
Timothy Malahy <TimothyMalahy@gmail.com>
2022-01-15 10:31:44 -0600

    settings

--- webpandas/settings.py
+++ webpandas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.21','webpandas.applikuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.21','webpandas.applikuapp.com', 'webpandas.org']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Application definition
